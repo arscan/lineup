173bccfa3dfb18b00b09e4754d99fb20a96baded
Rob Scanlon <robscanlon@gmail.com>
2015-05-31 19:31:19 -0400

    Loading updates.

--- js/LoadingPanel.js
+++ js/LoadingPanel.js
@@ -15,7 +15,11 @@ function createLoadingPanel(renderer, scale){
 
 var    donutMaterials = [],
        textureIndex = 0,
-       lastTextStartTime = 0;
+       lastTextStartTime = 0,
+       percent = 0,
+       started = false,
+       particleGroup,
+       particles = [];
 
    var panel = createPanel(renderer, width, height, {foregroundGlow: true});
 
@@ -33,23 +37,41 @@ var    donutMaterials = [],
            '}'
        ].join('\n'),
        fragmentShader: [
+           '#define PI 3.141592653589793238462643',
            'varying vec2 vUv;',
            'uniform float currentTime;',
            'uniform sampler2D tDiffuse;',
 
            'void main() {',
-           '  gl_FragColor = texture2D(tDiffuse, vUv);',
+           '  float radians = 2.0 * currentTime * PI;', 
+           '  gl_FragColor = vec4(0.0, 0.0, 0.0, 0.0);',
+
 
            '  if(vUv.x - .5 > 0.0){',
-           '    if((vUv.x - .5) / (vUv.y - .5) > currentTime){',
-           '      gl_FragColor = vec4(0.0, 0.0, 0.0, 0.0);',
+           '    if(currentTime > .5 || ((vUv.y - .5) / (vUv.x - .5) > cos(radians) / sin(radians))){',
+           '      gl_FragColor = texture2D(tDiffuse, vUv);',
            '    }',
            '  }',
-           '  if(vUv.x - .5 < 0.0){',
-           '    if((vUv.x - .5) / (vUv.y - .5) > currentTime){',
-           '      gl_FragColor = vec4(0.0, 0.0, 0.0, 0.0);',
+           '  if(vUv.x - .5 < 0.0 && currentTime > .5){',
+           '    if(currentTime >= 1.0 || ((vUv.y - .5) / (vUv.x - .5) > cos(radians) / sin(radians))){',
+           '      gl_FragColor = texture2D(tDiffuse, vUv);',
            '    }',
            '  }',
+           // '  if(vUv.x - .5 < 0.0){',
+           // '    if((vUv.y - .5) / (vUv.x - .5) < cos(PI + radians) / sin(PI + radians)){',
+           // '      gl_FragColor = vec4(0.0, 0.0, 0.0, 0.0);',
+           // '    }',
+           // '  }',
+           // '  if(vUv.x - .5 > 0.0 && vUv.y - .5 < 0.0){',
+           // '    if((vUv.x - .5) / (vUv.y - .5) > sin(radians) / cos(radians)){',
+           // '      gl_FragColor = vec4(0.0, 0.0, 0.0, 0.0);',
+           // '    }',
+           // '  }',
+           // '  if(vUv.x - .5 < 0.0){',
+           // '    if((vUv.x - .5) / (vUv.y - .5) > currentTime){',
+           // '      gl_FragColor = vec4(0.0, 0.0, 0.0, 0.0);',
+           // '    }',
+           // '  }',
 
            /*'  float bulletPercent = clamp((currentTime - bulletStartTime) / bulletDuration, 0.0, 1.0);',
            '  float textInPercent = clamp((currentTime - textInStartTime) / textInDuration, 0.0, 1.0);',
@@ -81,18 +103,16 @@ var    donutMaterials = [],
         var backgroundPlane = new THREE.Mesh( backgroundGeometry, backgroundMaterial );
         backgroundPlane.position.set(width/2, height/2, 0);
         panel.addToScene( backgroundPlane );
-        console.log(panel);
 
     }
 
     function createCircles(){
         var backgroundTexture = THREE.ImageUtils.loadTexture("images/loading-circles.png", undefined, LOADSYNC.register() );
-        var backgroundMaterial = new THREE.MeshBasicMaterial( {map: backgroundTexture, transparent: true } );
+        var backgroundMaterial = new THREE.MeshBasicMaterial( {map: backgroundTexture, transparent: true, color: 0xff8d07 } );
         var backgroundGeometry = new THREE.PlaneGeometry( 400 * scale, 400 * scale);
         var backgroundPlane = new THREE.Mesh( backgroundGeometry, backgroundMaterial );
         backgroundPlane.position.set(width/2, height/2, 0);
         panel.addToScene( backgroundPlane );
-        console.log(panel);
 
     }
 
@@ -112,24 +132,58 @@ var    donutMaterials = [],
         material.transparent = true;
         donutMaterials.push(material);
         var plane = new THREE.Mesh( geometry, material );
-        plane.position.set(height/2, width/2, 1);
+        plane.position.set(height/2, width/2, 10);
         panel.addToScene( plane );
 
     }
 
+   function createPointCloud(){
+
+       if(!particleGroup){
+           particleGroup = new THREE.Group();
+           particleGroup.applyMatrix( new THREE.Matrix4().makeTranslation( 200*scale, 200*scale, 0 ) );
+           panel.addToScene(particleGroup);
+       }
+
+       var geometry = new THREE.Geometry();
+       var sprite = THREE.ImageUtils.loadTexture( "images/links-particles.png", undefined, LOADSYNC.register() );
+
+       for(var i = 0; i< 250; i+=1){
+           var d = Math.random() * Math.PI * 2;
+           var l = Math.random() * 100 * scale + 100 * scale;
+           geometry.vertices.push( new THREE.Vector3(l*Math.cos(d), l*Math.sin(d), 5) );
+       }
+       // for (var  i = 0; i < 400; i+=30  ) {
+       //     for(var j = 0; j< 400; j+=30){
+
+       //         geometry.vertices.push( new THREE.Vector3(i, 100, 5) );
+       //     }
+       // }
+
+       var material = new THREE.PointCloudMaterial( { opacity: 1, size: 5 * scale, map: sprite, transparent: true, blending:THREE.AdditiveBlending, color: 0xff8d07/*0x10b0a0*/, sizeAttenuation: false } );
+
+
+       particles.push(new THREE.PointCloud( geometry, material ));
+       particleGroup.add(particles[particles.length-1]);
+
+   }
+
     function init(){
+
 
         // load images
 
         createBackground();
         createCircles();
+        for(var i =0; i< 5; i++){
+            createPointCloud();
+        }
+
 
         donutImages.forEach(function(url){
-            console.log(url);
             createDonut(url);
         });
 
-        console.log("GOT IT");
 
 
 
@@ -137,9 +191,26 @@ var    donutMaterials = [],
     }
 
     function render(time){
+
+        if(!started && time > 0){
+            new TWEEN.Tween({t: 0})
+                .to({t: 1}, 10000)
+                .onUpdate(function(){
+                    percent = this.t;
+            })
+            .start();
+
+            started = true;
+        }
         donutMaterials.forEach(function(donut, index){
-            donut.uniforms.currentTime.value = time - index * 10;
+            donut.uniforms.currentTime.value = percent;
         });
+
+        if(particleGroup){
+            particles.forEach(function(particles, index){
+                particles.rotation.z = -time * (.5 + index/10);
+            });
+        }
 
         /*
         if(lastTextStartTime + ROTATE_TIME < time){
